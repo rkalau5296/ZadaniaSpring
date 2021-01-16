51f9e81c9ca24136371fdd7ccfa93233fb8714ac
rkalau <rafal.kalata.itservice@gmail.com>
2021-01-17 00:25:16 +0100

    commit

---
+++ Zadanie2/src/main/java/zad2/ApplicationZad2.java
@@ -0,0 +1,11 @@
+package zad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationZad2 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationZad2.class, args);
+    }
+}
